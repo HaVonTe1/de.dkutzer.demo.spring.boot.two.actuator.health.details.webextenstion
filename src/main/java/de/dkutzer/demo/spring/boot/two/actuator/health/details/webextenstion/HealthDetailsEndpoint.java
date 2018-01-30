249d04e5a435d31682ce6ab28e1c5c1ebb4ab86e
Dirk Kutzer <dirk.kutzer@dbfuhrpark.com>
2018-01-30 13:12:17 +0100

    added custom endpoint

--- src/main/java/de/dkutzer/demo/spring/boot/two/actuator/health/details/webextenstion/HealthDetailsEndpoint.java
+++ src/main/java/de/dkutzer/demo/spring/boot/two/actuator/health/details/webextenstion/HealthDetailsEndpoint.java
@@ -1,31 +1,24 @@
 package de.dkutzer.demo.spring.boot.two.actuator.health.details.webextenstion;
 
 import java.util.Map;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.boot.actuate.endpoint.annotation.Selector;
 import org.springframework.boot.actuate.endpoint.web.WebEndpointResponse;
-import org.springframework.boot.actuate.endpoint.web.annotation.EndpointWebExtension;
 import org.springframework.boot.actuate.health.Health;
-import org.springframework.boot.actuate.health.HealthEndpoint;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.actuate.health.HealthStatusHttpMapper;
 import org.springframework.stereotype.Component;
-//its not possible to write your own HealthEndpointWebExtension
-//becaus of:
 
-/*
-Caused by: java.lang.IllegalStateException: Found two extensions for the same endpoint 'org.springframework.boot.actuate.health.HealthEndpoint': de.dkutzer.demo.spring.boot.two.actuator.health.details.webextenstion.HealthEndpointWebExtension and org.springframework.boot.actuate.health.HealthEndpointWebExtension
-
- */
 @Component
-@EndpointWebExtension(endpoint = HealthEndpoint.class)
-public class HealthEndpointWebExtension {
+@Endpoint(id = "healthdetails")
+public class HealthDetailsEndpoint {
 
     private HealthIndicator delegate;
 
     private final HealthStatusHttpMapper statusHttpMapper;
 
-    public HealthEndpointWebExtension(HealthIndicator delegate,
+    public HealthDetailsEndpoint(HealthIndicator delegate,
         HealthStatusHttpMapper statusHttpMapper) {
         this.delegate = delegate;
         this.statusHttpMapper = statusHttpMapper;
@@ -33,12 +26,12 @@ public class HealthEndpointWebExtension {
 
     //work in progress
     @ReadOperation
-    public WebEndpointResponse<Health> details(@Selector String details) {
+    public WebEndpointResponse<Health> healthdetails() {
         final Health health = this.delegate.health();
-        final Map<String, Object> d = health.getDetails();
-        System.out.println(d);
         Integer status = this.statusHttpMapper.mapStatus(health.getStatus());
         return new WebEndpointResponse<Health>(health, status);
     }
+
+
 
 }
--- src/main/java/de/dkutzer/demo/spring/boot/two/actuator/health/details/webextenstion/WebextenstionApplication.java
+++ src/main/java/de/dkutzer/demo/spring/boot/two/actuator/health/details/webextenstion/WebextenstionApplication.java
@@ -1,10 +1,34 @@
 package de.dkutzer.demo.spring.boot.two.actuator.health.details.webextenstion;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.endpoint.condition.ConditionalOnEnabledEndpoint;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.health.HealthStatusHttpMapper;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WebextenstionApplication {
+
+	@Bean
+	@ConditionalOnMissingBean
+	@ConditionalOnEnabledEndpoint
+	public HealthDetailsEndpoint myHealthEndpoint(HealthIndicator delegate,
+		HealthStatusHttpMapper statusHttpMapper) {
+		return new HealthDetailsEndpoint(delegate,
+			statusHttpMapper);
+	}
+//throws:
+	/*
+	Caused by: java.lang.IllegalStateException: Found two extensions for the same endpoint 'org.springframework.boot.actuate.health.HealthEndpoint': de.dkutzer.demo.spring.boot.two.actuator.health.details.webextenstion.HealthEndpointWebExtension and org.springframework.boot.actuate.health.HealthEndpointWebExtension
+
+	 */
+//	@Bean
+//	public HealthEndpointWebExtension myHealthEndpointExtension(HealthIndicator delegate,
+//		HealthStatusHttpMapper statusMapper){
+//		return new HealthEndpointWebExtension(delegate, statusMapper);
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebextenstionApplication.class, args);
